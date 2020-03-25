7cc87f429dd1dd820117f81730726cfb87a2a45a
Mingyu Kang <plenilune095@gmail.com>
2020-03-25 18:00:30 +0900

    BFS
    전형적인 BFS문제
    방문한 곳을 또 방문하지 않게(최소 변환 과정을 거치게) 설계하는 것이 중요

---
+++ programmers/level_3/단어 변환.py
@@ -0,0 +1,25 @@
+from collections import deque
+
+
+def solution(begin, target, words):
+    answer = 0
+    queue = deque([(list(begin), 0)])
+    N = len(begin)
+    visited = dict()
+
+    while queue:
+        now, depth = queue.popleft()
+        if ''.join(now) == target:
+            return depth
+        for i in range(N):
+            for j in range(97, 123):
+                temp = now[:]
+                if temp[i] != chr(j):
+                    temp[i] = chr(j)
+                    word = ''.join(temp)
+                    if word in words and visited.get(word) is None:
+                        visited[word] = True
+                        queue.append((temp, depth + 1))
+
+    return answer
+
