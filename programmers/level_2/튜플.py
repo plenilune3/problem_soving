ece5e9e9130d1dbaf908db3bed6c3671537c9f37
Mingyu Kang <plenilune095@gmail.com>
2020-04-01 01:47:47 +0900

    Simulation
    정규표현식을 활용한 시뮬레이션 문제
    Counter 자료구조를 활용

---
+++ programmers/level_2/튜플.py
@@ -0,0 +1,9 @@
+from collections import Counter
+import re
+
+
+def solution(s):
+    s = Counter(re.findall('\d+', s))
+    s = sorted(s.items(), key=lambda x: x[1], reverse=True)
+    return [int(k) for k, v in s]
+git
